adf4a6d06614f3a37cf1509df4e9ab26cbe9d16c
skar5268 <skar5268@gmail.com>
2024-05-21 19:33:36 +0800

    final

--- 02.個人品牌網站/all.js
+++ 02.個人品牌網站/all.js
@@ -1,21 +1,75 @@
-// core version + navigation, pagination modules:
-import Swiper from "swiper";
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+// RWD 設計，確認目前螢幕解析度
+const diWidth = screen.width
 
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+// DOM
+// menu
+const mobileMenu = document.querySelector(".mobile-menu")
+const menu = document.querySelector(".menu")
+// carousel
+const carouselCards = document.querySelector(".carousel-cards")
+const carouselCard = document.querySelectorAll(".carousel-card")
+const dot = document.querySelectorAll(".dot")
+// modal
+const openModal = document.querySelector("[data-open-modal]")
+const closeModal = document.querySelector("[data-close-modal]")
+const modal = document.querySelector("[data-modal-new]")
+
+// 手機板 MENU
+mobileMenu.addEventListener("click", function (e) {
+  menu.classList.toggle("d-block")
+})
+
+// Modal
+if(modal){
+  openModal.addEventListener("click", ()=>{
+    modal.classList.remove("d-none")
+  })
+  closeModal.addEventListener("click", ()=>{
+    modal.classList.add("d-none")
+  })
+  modal.addEventListener("click", (e)=>{
+    if(e.target === modal) modal.classList.add("d-none")
+  })  
+}
+
+
+// 首頁文章輪播效果
+let num = 0;
+
+if (diWidth > 375 && dot[0] && carouselCards) {
+  for (d = 0; d < dot.length; d++) dot[d].dataset.num = d
+
+  dot.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      let active = e.target.dataset.num
+      num = active - 1
+      changeCard()
+    });
+  });
+  dot[0].style.backgroundColor = "#000000";
+  let interval = setInterval(changeCard, 2500);
+}
+
+function changeCard() {
+  let margin = carouselCards.clientWidth * 0.05;
+
+  if (diWidth <= 767) {
+    margin = carouselCards.clientWidth * 0.02;
+    menu.classList.add("d-none");
+  }
+
+  num++;
+
+  if (num > carouselCard.length - 1) num = 1;
+  else if (num == carouselCard.length - 1) num = 0;
+
+  for (i = 0; i < carouselCard.length; i++)
+    carouselCard[i].style.transform = `translateX(${
+      -num * (carouselCard[i].clientWidth + margin)
+    }px)`;
+
+  for (y = 0; y < dot.length; y++) {
+    if (y == num) dot[y].style.backgroundColor = "#000000";
+    else dot[y].style.backgroundColor = "#E9E9E9";
+  }
+}
--- 02.個人品牌網站/dist/all.dev.js
+++ 02.個人品牌網站/dist/all.dev.js
@@ -1,29 +1,71 @@
 "use strict";
 
-var _swiper = _interopRequireDefault(require("swiper"));
+// RWD 設計，確認目前螢幕解析度
+var diWidth = screen.width; // DOM
+// menu
 
-var _modules = require("swiper/modules");
+var mobileMenu = document.querySelector(".mobile-menu");
+var menu = document.querySelector(".menu"); // carousel
 
-require("swiper/css");
+var carouselCards = document.querySelector(".carousel-cards");
+var carouselCard = document.querySelectorAll(".carousel-card");
+var dot = document.querySelectorAll(".dot"); // modal
 
-require("swiper/css/navigation");
+var openModal = document.querySelector("[data-open-modal]");
+var closeModal = document.querySelector("[data-close-modal]");
+var modal = document.querySelector("[data-modal-new]"); // 手機板 MENU
 
-require("swiper/css/pagination");
+mobileMenu.addEventListener("click", function (e) {
+  menu.classList.toggle("d-block");
+}); // Modal
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+if (modal) {
+  openModal.addEventListener("click", function () {
+    modal.classList.remove("d-none");
+  });
+  closeModal.addEventListener("click", function () {
+    modal.classList.add("d-none");
+  });
+  modal.addEventListener("click", function (e) {
+    if (e.target === modal) modal.classList.add("d-none");
+  });
+} // 首頁文章輪播效果
 
-// core version + navigation, pagination modules:
-// import Swiper and modules styles
-var swiper = new _swiper["default"]('.swiper-container', {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
+
+var num = 0;
+
+if (diWidth > 375 && dot[0] && carouselCards) {
+  for (d = 0; d < dot.length; d++) {
+    dot[d].dataset.num = d;
   }
-});
+
+  dot.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      var active = e.target.dataset.num;
+      num = active - 1;
+      changeCard();
+    });
+  });
+  dot[0].style.backgroundColor = "#000000";
+  var interval = setInterval(changeCard, 2500);
+}
+
+function changeCard() {
+  var margin = carouselCards.clientWidth * 0.05;
+
+  if (diWidth <= 767) {
+    margin = carouselCards.clientWidth * 0.02;
+    menu.classList.add("d-none");
+  }
+
+  num++;
+  if (num > carouselCard.length - 1) num = 1;else if (num == carouselCard.length - 1) num = 0;
+
+  for (i = 0; i < carouselCard.length; i++) {
+    carouselCard[i].style.transform = "translateX(".concat(-num * (carouselCard[i].clientWidth + margin), "px)");
+  }
+
+  for (y = 0; y < dot.length; y++) {
+    if (y == num) dot[y].style.backgroundColor = "#000000";else dot[y].style.backgroundColor = "#E9E9E9";
+  }
+}
